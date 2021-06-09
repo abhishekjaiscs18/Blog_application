6917eeb6804bba339b3f0ed15972ec45f1cca9a0
abhishekjais <abhishek.jaiswal_cs18@gla.ac.in>
2021-06-10 01:07:18 +0530

    Fixes

--- front-end/src/components/parts/Posts.js
+++ front-end/src/components/parts/Posts.js
@@ -1,27 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import PostItem from "./PostItem";
 import Pagination from "./Pagination";
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
 
-    const posts  = useSelector((state) => state.posts);
-    const dispatch = useDispatch();
+  const postItems = posts.map((post) => <PostItem post={post} />);
 
-    console.log(posts);
-    return(
-        <div className="col-lg-8">
-			<div className="blog-box list-style">
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                
-                <Pagination />
-			</div>
-		</div>
-    );
+  return (
+    <div className="col-lg-8">
+      <div className="blog-box list-style">
+        {postItems}
+        <Pagination />
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
